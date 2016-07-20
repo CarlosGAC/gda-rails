614aff6a039637f6ea324fece6e20833af8c189e
Carlos Alberto Garcia de Alba Chavez <carlos.alberto.gdea.ch@gmail.com>
2016-07-20 13:39:04 -0500

    Agregadas seeds para desarrolladores frontend
    Se agregaron seeds y modificaron las existentes, para el desarrollo del
    frontend

--- control-gda/db/seeds.rb
+++ control-gda/db/seeds.rb
@@ -28,24 +28,21 @@ trucks_list.each do |number, classification, description|
 end
 
 service_list = [
-  ["2016-07-19","2000-01-01 23:12:00","Usuario","Colima","Guadalajara","1234","19","Chevrolet","Aveo","Rojo","JBC2180","2012", 3, 1],
-  ["2016-05-02","2000-01-01 02:15:00","PGR","Ciudad Guzman","Autlan","2534","1","Ford","Focus","Rojo","JAD2121","2005", 1, 2],
-  ["2016-05-02","2000-01-01 02:15:00","PGR","Autlan","Zapotiltic","2534","1","Ford","Focus","Rojo","JAD2121","2005", 1, 2]
+  ["2016-07-19","2000-01-01 23:12:00","Usuario","Colima","Guadalajara","1234","19","Chevrolet","Aveo","Rojo","JBC2180","2012", 3, 1,1],
+  ["2016-05-02","2000-01-01 02:15:00","PGR","Ciudad Guzman","Autlan","2534","1","Ford","Focus","Rojo","JAD2121","2005", 1, 2,2],
+  ["2016-05-02","2000-01-01 02:15:00","PGR","Autlan","Zapotiltic","2534","1","Ford","Focus","Rojo","JAD2121","2005", 1, 2,3]
 ]
 
-service_list.each do |date, hour, client, ubication, destiny, inventary_num, keys_num, brand, car_type, color, licence_plates, model, operator_id, truck_id|
-  Service.create(date: date, hour: hour, client: client, ubication: ubication, destiny: destiny, inventary_num: inventary_num, keys_num: keys_num, brand: brand, car_type: car_type, color: color, licence_plates: licence_plates, model: model, operator_id: operator_id, truck_id: truck_id)
+service_list.each do |date, hour, client, ubication, destiny, inventary_num, keys_num, brand, car_type, color, licence_plates, model, operator_id, truck_id, assistance_id|
+  Service.create(date: date, hour: hour, client: client, ubication: ubication, destiny: destiny, inventary_num: inventary_num, keys_num: keys_num, brand: brand, car_type: car_type, color: color, licence_plates: licence_plates, model: model, operator_id: operator_id, truck_id: truck_id, assistance_id: assistance_id)
 end
 
-#t.date :date
-#t.time :hour
-#t.string :client
-#t.string :ubication
-#t.string :destiny
-#t.string :inventary_num
-#t.string :keys_num
-#t.string :brand
-#t.string :car_type
-#t.string :color
-#t.string :licence_plates
-#t.string :model
+assistance_list = [
+  ["12361490", "Mario Gomez", "Jose Treviño", "1200", "5000", "12:05", "15:06", "Carlos", "Nope"],
+  ["15290843", "Juan Perez", "Luis Lopez", "0", "1000", "13:45", "14:00", "Blanca", "Nel carnal"],
+  ["1216094635", "Daniel Jimenez", "Mario Garza", "130", "10000", "01:32", "02:50", "Lucio", "Eh we te gusta maincra?"]
+]
+
+assistance_list.each do |record_number, company_user, affiliate_name, affiliate_pays, company_pays, contact_time, end_time, who_quoted, comments|
+  Assistance.create(record_number: record_number, company_user: company_user, affiliate_name: affiliate_name, affiliate_pays: affiliate_pays, company_pays: company_pays, contact_time: contact_time, end_time: end_time, who_quoted: who_quoted, comments: comments)
+end
