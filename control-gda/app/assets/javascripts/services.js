884a3de6602cbc0cf3b366739461571f05f57580
Racso35352 <racso35352@gmail.com>
2016-08-16 10:45:57 -0500

    Modificación a las paginas Nuevo Servicio y Nueva Asistencia
    Las pestañas de Nuevo servicio y Nueva Asistencia ya tienen formato,
    usando la clase list3 en el archivo base.styl.

---
+++ control-gda/app/assets/javascripts/services.js
@@ -0,0 +1,13 @@
+(function() {
+  jQuery(function() {
+    $('#df_start').datepicker({
+      dateFormat: 'yy-mm-dd'
+    });
+    jQuery(function() {});
+    return $('#df_end').datepicker({
+      dateFormat: 'yy-mm-dd'
+    });
+  });
+
+}).call(this);
+
