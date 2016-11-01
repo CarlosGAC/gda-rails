847e973bbc245933ec4d50ec9305bc51e2da0d26
Carlos Alberto Garcia de Alba Chavez <carlos.alberto.gdea.ch@gmail.com>
2016-10-31 20:34:21 -0600

    Join en Asistencias
    Join y fixes del bundler de Adrian

--- control-gda/app/models/assistance.rb
+++ control-gda/app/models/assistance.rb
@@ -1,5 +1,9 @@
 class Assistance < ActiveRecord::Base
   has_one :service
+
+  def self.today()
+  	Assistance.joins(:service).where(services: {date: Date.today})
+  end
   # TO BE FIXED
   #def self.today
   	#where('created_at == ?', Date.today)
--- control-gda/app/models/service.rb
+++ control-gda/app/models/service.rb
@@ -2,7 +2,7 @@ class Service < ActiveRecord::Base
   belongs_to :truck
   belongs_to :operator
   belongs_to :assistance
-  
+
   def self.today()
   	where('date == ?', Date.today)
   end
