37d74ea2b29aeffbf95141b7829303485fe06db8
Carlos Alberto García de Alba Chávez <carlos.alberto.gdea.ch@gmail.com>
2017-06-05 13:23:28 -0500

    Limpieza y correccion de bugs
    Se corrigieron bugs en la seccion de Servicio y Grua.
    Se eliminó Doorkeeper del sistema

--- control-gda/app/controllers/trucks_controller.rb
+++ control-gda/app/controllers/trucks_controller.rb
@@ -8,12 +8,25 @@ class TrucksController < ApplicationController
   end
 
   def create
-    @truck = Truck.new(number: params[:truck][:number], classification: params[:classification], description: params[:description])
-
+    @truck = Truck.new(number: params[:truck][:number], classification: params[:classification], description: params[:truck][:description])
     if @truck.save
       redirect_to trucks_url, notice: "Grua guardada satisfactoriamente"
     else
       render :new, error: "Eror: No se pudo guardar la grua satisfactoriamente"
     end
   end
+
+  def edit
+    @truck = Truck.find(params[:id])
+  end
+
+  def update
+    @truck = Truck.find(params[:id])
+    if @truck.update(number: params[:truck][:number], classification: params[:classification], description: params[:truck][:description])
+      redirect_to trucks_url, notice: "Grua editada satisfactoriamente"
+    else
+      render :edit, error: "Error: La grua no se pudo editar satisfactoriamente"
+    end
+  end
+
 end
--- control-gda/config/routes.rb
+++ control-gda/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  use_doorkeeper
   root 'welcome#index'
 
   resources :users
@@ -14,10 +13,5 @@ Rails.application.routes.draw do
   resources :assistances
 
   resources :map
-
-  #============= API =============#
-  namespace :api, defaults: {format:"json"} do
-    resources :services, controller: :services, only: [:index]
-  end
 
 end
